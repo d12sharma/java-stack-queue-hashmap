237a32a8078bdee48c964cf78e33855867dd4a1e
dhruvsharma123 <dhruv1931.be21@chitkara.edu.in>
2025-03-24 22:27:00 +0530

    Dhruv Sharma Add:consecutive sequence

---
+++ LongestConsecutiveSequence.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class LongestConsecutiveSequence {
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longestStreak = 0;
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) { // Check if it's the start of a sequence
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (numSet.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+        return longestStreak;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {90, 5, 30, 4, 1, 3, 2};
+        System.out.println(longestConsecutive(arr));
+    }
+}
